8db8369fd76c7d5f339f7f2fb28807e4d17c5271
Aitor Porro <89.aitor.porro@gmail.com>
2023-03-08 21:02:02 +0100

    feat: Add config to publish it.

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
               avatar={<img src="https://play-lh.googleusercontent.com/ELLR6rcIP_mr6pB4kX9QhBKF-najkWHfb8RqceX4CBsyel3o_W9DoGas7WfPgfiIsQ" alt="Hbo" />}
               username={<span><strong>HBO</strong> @HBO</span>}
               body={<p>Patiently waiting for March 26. <a href="#">#succession</a></p>}
-              image={<img src="https://productplacementblog.com/wp-content/uploads/2021/11/Apple-iPad-Pro-Tablet-of-Jeremy-Strong-as-Kendall-Roy-in-Succession-S03E07-Too-Much-Birthday-2021.jpg" alt="tweet image" />}
+              image={<img src="https://productplacementblog.com/wp-content/uploads/2021/11/Apple-iPad-Pro-Tablet-of-Jeremy-Strong-as-Kendall-Roy-in-Succession-S03E07-Too-Much-Birthday-2021.jpg" alt="description" />}
               actions={[
                 <Button theme="transparent" size="small">Reply</Button>,
                 <Button theme="transparent" size="small">Retweet</Button>,
@@ -53,7 +53,7 @@ function App() {
             <Card
               layout="instagram"
               body={<p>This is the body of the card.</p>}
-              image={<img src="https://productplacementblog.com/wp-content/uploads/2021/11/Apple-iPad-Pro-Tablet-of-Jeremy-Strong-as-Kendall-Roy-in-Succession-S03E07-Too-Much-Birthday-2021.jpg" alt="tweet image" />}
+              image={<img src="https://productplacementblog.com/wp-content/uploads/2021/11/Apple-iPad-Pro-Tablet-of-Jeremy-Strong-as-Kendall-Roy-in-Succession-S03E07-Too-Much-Birthday-2021.jpg" alt="description" />}
               actions={[
                 <Button theme="transparent" size="small">Like</Button>,
                 <Button theme="transparent" size="small">Share</Button>,
---
+++ src/lib/components/index.js
@@ -0,0 +1,3 @@
+import Card from './components/Card/Card';
+import Button from './components/Button/Buttom';
+export { Card, Button };
